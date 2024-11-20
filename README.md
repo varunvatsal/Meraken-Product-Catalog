f5cfcf364563898f96ae3926eb915055130dc067
varunvatsal <varunvatsal963@gmail.com>
2024-11-20 11:04:33 +0530

    added Search feature and made changes to readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-stock photos taken from pexels
+
+# How to Run on Local
+### npm install
+### npm run dev
+
+## stock photos taken from pexels
+
+# card and accordian taken from bootstrap
+## instruction foolowed from this site -> https://react-bootstrap.netlify.app/docs/getting-started/introduction
+## for cards -> https://react-bootstrap.netlify.app/docs/components/cards
+## for accordian -> https://react-bootstrap.netlify.app/docs/components/accordion
+
+
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form';
+
+const Search = ({product}) => {
+  let {productList, setProductList} = product
+  console.log(productList)
+  let [searchVal, setSearchVal] = useState("")
+  let handleChange = (e) => {
+    if(e.target.value === ""){
+      setProductList((prev) => {
+        return prev.map((ele) => {
+          return {...ele, visible: true}
+        })
+      })
+    }else {
+      setProductList((prev) => {
+        return prev.map((ele) => {
+          return {...ele, visible: ele.name.toLowerCase().startsWith(e.target.value.toLowerCase()) ? true : false}
+        })
+      })
+    }
+    setSearchVal(e.target.value)
+  }
+  return (
+    <>
+      <Form.Control size="lg" type="text" placeholder="Search product" value={searchVal} onChange={handleChange}/>
+    </>
+  )
+}
+
+export default Search
